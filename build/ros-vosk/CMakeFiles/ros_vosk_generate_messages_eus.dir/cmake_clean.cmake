6ebe62bbe028eb934a96d771ef3468f7dfe9c930
Vikas-106 <iamvikas558@gmail.com>
2024-05-02 11:53:59 +0530

    Added navigation code

---
+++ build/ros-vosk/CMakeFiles/ros_vosk_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/vikas/Restaurant_ws/devel/share/roseus/ros/ros_vosk/manifest.l"
+  "/home/vikas/Restaurant_ws/devel/share/roseus/ros/ros_vosk/msg/speech_recognition.l"
+  "CMakeFiles/ros_vosk_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ros_vosk_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
